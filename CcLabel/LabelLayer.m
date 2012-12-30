6366c17480e63eaea851893831a4c7a47dacc2cb
IanFan <ianfan0704@gmail.com>
2012-12-30 10:25:33 +0800

    Make "JUMP INTO CONCLUSION" looks like "TUMB  TNTQ  CQNCLUSJQN" for fun.

--- CcLabel/LabelLayer.m
+++ CcLabel/LabelLayer.m
@@ -26,13 +26,13 @@
   CGSize winSize = [CCDirector sharedDirector].winSize;
   
   //create TTFLabel with standard setting
-  CCLabelTTF *standardLabel = [CCLabelTTF labelWithString:@"CCLabelTTF with standard setting" fontName:@"Helvetica" fontSize:28];
+  CCLabelTTF *standardLabel = [CCLabelTTF labelWithString:@"Standard CCLabelTTF" fontName:@"Helvetica" fontSize:30];
   [standardLabel setPosition:ccp(winSize.width/2, winSize.height/2 + 80)];
   [self addChild:standardLabel];
   
   //create TTFLabel with dimensions, alignment, lineBreakMode
-//    CCLabelTTF *leftLabel = [CCLabelTTF labelWithString:@"dimensions, alignment, lineBreakMode" fontName:@"Helvetica" fontSize:32 dimensions:CGSizeMake(520, 32)  hAlignment:kCCTextAlignmentCenter]; //same method in different Cocos2D version
-  CCLabelTTF *dimensionLabel = [CCLabelTTF labelWithString:@"CCLabelTTF with dimensions, alignment, lineBreakMode" dimensions:CGSizeMake(winSize.width, 16) hAlignment:kCCTextAlignmentCenter fontName:@"Helvetica" fontSize:18];
+//    CCLabelTTF *leftLabel = [CCLabelTTF labelWithString:@"dimensions, alignment, lineBreakMode" fontName:@"Helvetica" fontSize:30 dimensions:CGSizeMake(520, 32)  hAlignment:kCCTextAlignmentCenter]; //same method in different Cocos2D version
+  CCLabelTTF *dimensionLabel = [CCLabelTTF labelWithString:@"TUMB  TNTQ  CQNCLUSJQN" dimensions:CGSizeMake(winSize.width, 16) hAlignment:kCCTextAlignmentCenter fontName:@"Times New Roman" fontSize:30];
   [dimensionLabel setPosition:ccp(winSize.width/2, winSize.height/2)];
   [self addChild:dimensionLabel];
   
@@ -40,18 +40,11 @@
   //1. Add .ttf file to the project (check "outback.ttf" in MyResources/Font folder in Xcode)
   //2. Add "Fonts provided by application" in "info.plist" (in Resources folder), type FontName.ttf in item ("outback.ttf" in this case)
   //Check the "addFontInInfoPlist.png" to know how to do step "2.".
-  CCLabelTTF *customFontLabel = [CCLabelTTF labelWithString:@"CCLabelTTF with a custom font" fontName:@"outback" fontSize:28];
+  CCLabelTTF *customFontLabel = [CCLabelTTF labelWithString:@"I am Custom Font" fontName:@"outback" fontSize:30];
   [customFontLabel setColor:ccc3(255,255,0)];
   [customFontLabel setPosition:ccp(winSize.width/2, winSize.height/2 - 80)];
   [customFontLabel setAnchorPoint:ccp(0.5f, 0.5f)];
   [self addChild:customFontLabel];
-  
-  //detect the device if it is an iPhone or iPad
-  if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
-    standardLabel.fontSize = 24;
-    dimensionLabel.fontSize = 18;
-    customFontLabel.fontSize = 24;
-  }
 }
 
 #pragma mark -
